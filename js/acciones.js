6c0c6d7f1f93e0557b985c1e942c798c39f76e3e
AL22F13 <cotadmc@gmail.com>
2015-06-24 11:35:03 -0500

    js
    js

---
+++ js/acciones.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+  $('#btnmenu').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+
+
+ $('#btnControversia').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+  
+   $('#btnDiscografia').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+  
+   $('#btnMiembros').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+  
+   $('#btndatos').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+  
+   $('#btnhistoria').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+  
+  
+   $('#btnprincipal').on('tap',function(){
+	  navigator.vibrate(1000);
+  });
+  
+  
+}); 
+});
+
